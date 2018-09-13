0e7eefc8200c04ef55fbccfc909ce0ae39a66959
Oliver <oliver--conquest@hotmail.com>
2018-09-13 14:47:14 +1000

    Visual studio git commit

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 
 Array = [75,81,78,71,89,68,69,65,64,66,79,82,69,76,75,81,67,53,54,59,70,68,68,68]
-S=0
+Sa=0
 
 for i in range(0,len(Array)):
-    S = S+Array[i]*3/(len(Array)*3)
+    Sa = Sa+Array[i]*3/(len(Array)*3)
 
-print(S)
+print(Sa)
