e01a1bc0c4a1377fa6cd99f72f4b094b24c1735b
Oliver <oliver--conquest@hotmail.com>
2018-09-13 14:49:44 +1000

    Another try

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 
 Array = [75,81,78,71,89,68,69,65,64,66,79,82,69,76,75,81,67,53,54,59,70,68,68,68]
-Sa=0
+S=0
 
 for i in range(0,len(Array)):
-    Sa = Sa+Array[i]*3/(len(Array)*3)
+    S = S+Array[i]*3/(len(Array)*3)
 
-print(Sa)
+print(S)
